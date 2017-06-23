67312847a85821881823b9862bb588d901b88e68
Arnab Wahid <arnabwahid@gmail.com>
2017-06-24 05:45:15 +0600

    archive.php3

--- archive.php
+++ archive.php
@@ -24,32 +24,32 @@ get_header(); ?>
 						</h1>
 					<?php } elseif ( is_author() ) { ?>
 						<h1 class="archive_title h2">
-							<span><?php esc_html_e( 'Posts By:', 'wpbootstrap' ); ?></span> <?php get_the_author_meta('display_name'); ?>
+							<span><?php esc_html_e( 'Posts By:', 'wpbootstrap' ); ?></span> <?php get_the_author_meta( 'display_name' ); ?>
 						</h1>
 					<?php } elseif ( is_day() ) { ?>
 						<h1 class="archive_title h2">
-							<span><?php esc_html_e( 'Daily Archives:', 'wpbootstrap' ); ?></span> <?php the_time('l, F j, Y'); ?>
+							<span><?php esc_html_e( 'Daily Archives:', 'wpbootstrap' ); ?></span> <?php the_time( 'l, F j, Y' ); ?>
 						</h1>
 					<?php } elseif ( is_month() ) { ?>
 						<h1 class="archive_title h2">
-							<span><?php esc_html_e( 'Monthly Archives:', 'wpbootstrap' ); ?></span> <?php the_time('F Y'); ?>
+							<span><?php esc_html_e( 'Monthly Archives:', 'wpbootstrap' ); ?></span> <?php the_time( 'F Y' ); ?>
 						</h1>
 					<?php } elseif ( is_year() ) { ?>
 						<h1 class="archive_title h2">
-							<span><?php esc_html_e( 'Yearly Archives:', 'wpbootstrap' ); ?></span> <?php the_time('Y'); ?>
-					    </h1>
+							<span><?php esc_html_e( 'Yearly Archives:', 'wpbootstrap' ); ?></span> <?php the_time( 'Y' ); ?>
+						</h1>
 					<?php } ?>
 					</div>
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 						
 						<header>
 							
 							<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 							
-							<p class="meta"><?php esc_html_e( "Posted", 'wpbootstrap' ); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php esc_html_e( "by", 'wpbootstrap' ); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php esc_html_e( "filed under", 'wpbootstrap' ); ?> <?php the_category(', '); ?>.</p>
+							<p class="meta"><?php esc_html_e( 'Posted', 'wpbootstrap' ); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php esc_html_e( 'by', 'wpbootstrap' ); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php esc_html_e( 'filed under', 'wpbootstrap' ); ?> <?php the_category(', '); ?>.</p>
 						
 						</header> <!-- end article header -->
 					
@@ -69,15 +69,15 @@ get_header(); ?>
 					
 					<?php endwhile; ?>	
 					
-					<?php if (function_exists('wp_bootstrap_page_navi')) { // if expirimental feature is active. ?>
+					<?php if ( function_exists( 'wp_bootstrap_page_navi' ) ) { // if expirimental feature is active. ?>
 						
 						<?php wp_bootstrap_page_navi(); // use the page navi function. ?>
 
 					<?php } else { // if it is disabled, display regular wp prev & next links. ?>
 						<nav class="wp-prev-next">
 							<ul class="pager">
-								<li class="previous"><?php next_posts_link(_e('&laquo; Older Entries', 'wpbootstrap' )) ?></li>
-								<li class="next"><?php previous_posts_link(_e('Newer Entries &raquo;', 'wpbootstrap' )) ?></li>
+								<li class="previous"><?php next_posts_link( esc_html_e( '&laquo; Older Entries', 'wpbootstrap' ) ) ?></li>
+								<li class="next"><?php previous_posts_link( esc_html_e( 'Newer Entries &raquo;', 'wpbootstrap' ) ) ?></li>
 							</ul>
 						</nav>
 					<?php } ?>
@@ -87,10 +87,10 @@ get_header(); ?>
 					
 					<article id="post-not-found">
 						<header>
-							<h1><?php esc_html_e( "No Posts Yet", 'wpbootstrap' ); ?></h1>
+							<h1><?php esc_html_e( 'No Posts Yet', 'wpbootstrap' ); ?></h1>
 						</header>
 						<section class="post_content">
-							<p><?php esc_html_e( "Sorry, What you were looking for is not here.", 'wpbootstrap' ); ?></p>
+							<p><?php esc_html_e( 'Sorry, What you were looking for is not here.', 'wpbootstrap' ); ?></p>
 						section>
 						<footer>
 						</footer>
@@ -100,8 +100,8 @@ get_header(); ?>
 			
 				</div> <!-- end #main -->
 
-				<?php get_sidebar(); // sidebar 1 ?>
-    
+				<?php get_sidebar(); // sidebar 1. ?>
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
