be9cc37e97cf8c1eda223d705078cfd03d8b2e5f
Arnab Wahid <arnabwahid@gmail.com>
2017-06-26 13:36:07 +0600

    function.php
    Line 1-100 revise.

--- functions.php
+++ functions.php
@@ -10,22 +10,22 @@ if ( is_readable( $locale_file ) ) require_once( $locale_file );
 if ( ! function_exists( 'wp_bootstrap_head_cleanup' ) ) {
   function wp_bootstrap_head_cleanup() {
     // remove header links.
-    remove_action( 'wp_head', 'feed_links_extra', 3 );                    // Category Feeds
-    remove_action( 'wp_head', 'feed_links', 2 );                          // Post and Comment Feeds
-    remove_action( 'wp_head', 'rsd_link' );                               // EditURI link
-    remove_action( 'wp_head', 'wlwmanifest_link' );                       // Windows Live Writer
-    remove_action( 'wp_head', 'index_rel_link' );                         // index link
-    remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );            // previous link
-    remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );             // start link
-    remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Links for Adjacent Posts
-    remove_action( 'wp_head', 'wp_generator' );                           // WP version
+    remove_action( 'wp_head', 'feed_links_extra', 3 );                    // Category Feeds.
+    remove_action( 'wp_head', 'feed_links', 2 );                          // Post and Comment Feeds.
+    remove_action( 'wp_head', 'rsd_link' );                               // EditURI link.
+    remove_action( 'wp_head', 'wlwmanifest_link' );                       // Windows Live Writer.
+    remove_action( 'wp_head', 'index_rel_link' );                         // index link.
+    remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );            // previous link.
+    remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );             // start link.
+    remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Links for Adjacent Posts.
+    remove_action( 'wp_head', 'wp_generator' );                           // WP version.
   }
 }
 // Launch operation cleanup.
 add_action( 'init', 'wp_bootstrap_head_cleanup' );
 
 // remove WP version from RSS.
-if ( ! function_exists( 'wp_bootstrap_rss_version' ) ) {  
+if ( ! function_exists( 'wp_bootstrap_rss_version' ) ) {
   function wp_bootstrap_rss_version() { return ''; }
 }
 add_filter( 'the_generator', 'wp_bootstrap_rss_version' );
@@ -34,7 +34,7 @@ add_filter( 'the_generator', 'wp_bootstrap_rss_version' );
 if ( ! function_exists( 'wp_bootstrap_excerpt_more' ) ) {
   function wp_bootstrap_excerpt_more( $more ) {
     global $post;
-    return '...  <a href="'. get_permalink( $post->ID ) . '" class="more-link" title="Read ' . get_the_title( $post->ID ) . '">Read more &raquo;</a>';
+    return '...  <a href="' . get_permalink( $post->ID ) . '" class="more-link" title="Read ' . get_the_title( $post->ID ) . '">Read more &raquo;</a>';
   }
 }
 add_filter( 'excerpt_more', 'wp_bootstrap_excerpt_more' );
@@ -49,7 +49,7 @@ if ( ! function_exists( 'wp_bootstrap_theme_support' ) ) {
 
     // Add post format support - if these are not needed, comment them out.
       add_theme_support( 'post-formats',      // post formats.
-      array( 
+      array(
         'aside',   // title less blurb.
         'gallery', // gallery of images.
         'link',    // quick link to other site.
